f802c296d7c95bb6f9aec18f407a893a2148a869
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-05 11:15:20 +0530

    Simple Addition Program using scanf function in C

---
+++ Lecture 04/program08.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+// Function to calculate the sum of two numbers
+int Addition(int No1, int No2)
+{
+    int sum = 0;                // Declare and initialize sum to 0
+    sum = No1 + No2;            // Calculate the sum of the two numbers
+    return sum;                 // Return the sum to the caller
+}
+
+int main()
+{
+    int value1 = 11, value2 = 22;  // Initializing two local variables with some values
+    int Ret = 0;                    // To store the result of the addition
+
+    // Taking input from the user for the two values
+    printf("Enter the First Value :");
+    scanf("%d", &value1);                   // Read the first number
+    printf("Enter the Second Value :");
+    scanf("%d", &value2);                   // Read the second number
+
+    // Call the Addition function with user-provided values
+    Ret = Addition(value1, value2);
+
+    // Print the result of the addition
+    printf("Addition is  : %d \n", Ret);
+
+    return 0;
+}
