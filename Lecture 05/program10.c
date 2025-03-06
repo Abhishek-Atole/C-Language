6363c4f3c0a3becd706d30bb397fedbc3014972c
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-06 09:41:25 +0530

    Accept Number form the user and check whether that the number is odd or even

--- Lecture 05/program10.c
+++ Lecture 05/program10.c
@@ -1,22 +1,25 @@
 //Accept Number form the user and check whether that the number is odd or even
-#include<stdio.h>
+#include<stdio.h>   /*# = preprocessor directive    
+                      include = use to include the header file
+                      stdio.h= standard inout output files */
 int main()
 {
-    int No=0;
-    int Ans=0;
+    int No = 0;                      // Declare an integer variable to store the user's input
+    int Ans = 0;                     // Declare an integer variable to store the result of the modulus operation
 
-    printf("Enter Number :");
-    scanf("%d",&No);
+    printf("Enter Number: ");          // Prompt the user to enter a number
+    scanf("%d", &No);                 // Read the user's input into the variable No
 
-    Ans = No % 2;
-
-    if(Ans == 0)
+    Ans = No % 2;                      // Calculate the remainder when No is divided by 2
+    
+    if(Ans == 0)                         // If the remainder is 0, the number is even
     {
-        printf("Number is Even \n");
+        printf("Number is Even \n");     // Print "Number is Even"
     }
     else
     {
-        printf("Number is odd \n");
+        printf("Number is Odd \n");     // Print "Number is Odd"
     }
-    return 0;
+
+    return 0;                            // Return 0 to indicate successful execution
 }
