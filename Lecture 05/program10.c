de957122bbbe17131b39fcb7604618ff1b7ffc90
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-06 09:49:16 +0530

    Accept Number form the user and check whether that the number is odd or even

--- Lecture 05/program10.c
+++ Lecture 05/program10.c
@@ -1,24 +1,27 @@
-//Accept Number form the user and check whether that the number is odd or even
-#include<stdio.h>   /*# = preprocessor directive    
-                      include = use to include the header file
-                      stdio.h= standard inout output files */
+#include<stdio.h>   /* Preprocessor directive to include the standard input-output header file */
+
 int main()
 {
     int No = 0;                      // Declare an integer variable to store the user's input
     int Ans = 0;                     // Declare an integer variable to store the result of the modulus operation
 
-    printf("Enter Number: ");          // Prompt the user to enter a number
+    // Prompt the user to enter a number
+    printf("Enter Number: ");
     scanf("%d", &No);                 // Read the user's input into the variable No
 
-    Ans = No % 2;                      // Calculate the remainder when No is divided by 2
-    
-    if(Ans == 0)                         // If the remainder is 0, the number is even
+    Ans = No % 2;                      // Calculate the remainder when No is divided by 2 and store it in Ans
+
+    // Print the value of Ans (remainder of the division)
+    /*printf("Remainder (Ans) is: %d \n", Ans);  // Display the remainder after division by 2*/
+
+    // Check if the remainder is 0; if so, the number is even
+    if(Ans == 0)                     
     {
-        printf("Number is Even \n");     // Print "Number is Even"
+        printf("Number is Even \n");     // If remainder is 0, the number is Even
     }
     else
     {
-        printf("Number is Odd \n");     // Print "Number is Odd"
+        printf("Number is Odd \n");      // Otherwise, the number is Odd
     }
 
     return 0;                            // Return 0 to indicate successful execution
