61f84fdc74dad398400dd6ede1e3dc2563af5c91
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-07 19:51:05 +0530

    For Loop (Iteration)

--- Lecture 06/iteration1.c
+++ Lecture 06/iteration1.c
@@ -1,10 +1,14 @@
+//For loop code
 #include<stdio.h>
+
 int main()
 {
-    int i=0;
-    for(i=3;i<=5;i++)
+    int i = 0;  // Declare and initialize i
+
+    for(i = 1; i <= 10; i++)  // Loop from 3 to 5 (inclusive)
     {
-        printf("Jay Ganesh .. %d \n",i);
+        printf("Jay Ganesh .. %d \n", i);  // Print message with i
     }
-    return 0;
+
+    return 0;  // Return success
 }
