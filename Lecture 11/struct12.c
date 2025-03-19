a0707120cbb820040cf7764ed68dd2ead70aceca
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-19 16:16:30 +0530

    Code of Memmory Management policy for structure

---
+++ Lecture 11/struct12.c
@@ -0,0 +1,16 @@
+#include<stdio.h.>
+struct Demo 
+{
+    int i;
+    char ch; 
+    float f;
+    double d;
+
+};
+
+int main()
+{
+    struct Demo obj;
+    printf("Size of the Object is : %lu\n",sizeof(obj));
+    return 0;
+}
