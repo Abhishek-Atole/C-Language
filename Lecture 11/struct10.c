c4b763b530ba424204a46e90651f51fae614d42f
Abhishek-Atole <abhiatole17@gmail.com>
2025-03-19 08:39:24 +0530

    struct and unions

--- Lecture 11/struct10.c
+++ Lecture 11/struct10.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-struct Hello 
+#include <stdio.h>
+struct Hello
 {
     int no;
     float d;
@@ -10,25 +10,25 @@ struct Demo
     int i;
     float f;
     struct Hello hobj;
-    
-}dobj;
+
+} dobj;
 
 int main()
 {
-        struct Hello obj;
+    struct Hello obj;
 
-    dobj.i=11;
-    dobj.f=90.99;
+    dobj.i = 11;
+    dobj.f = 90.99;
 
-    dobj.hobj.no=21;
-    dobj.hobj.d=90.88;
+    dobj.hobj.no = 21;
+    dobj.hobj.d = 90.88;
 
-    printf("size of the object is : %d\n",sizeof(dobj));
+    printf("size of the object is : %d\n", sizeof(dobj));
 
-    printf("%d\n",dobj.i);
-    printf("%f\n",dobj.f);
-    printf("%d\n",dobj.hobj.no);
-    printf("%f\n",dobj.hobj.d);
+    printf("%d\n", dobj.i);
+    printf("%f\n", dobj.f);
+    printf("%d\n", dobj.hobj.no);
+    printf("%f\n", dobj.hobj.d);
 
     return 0;
 }
